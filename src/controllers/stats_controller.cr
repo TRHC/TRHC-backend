eb21121c3a8af7710596366ae31fe79728a0be85
Grig Betsan <griggors@gmail.com>
2018-12-03 23:28:33 +0200

    Reformat stats getter and add detailed information

--- src/controllers/stats_controller.cr
+++ src/controllers/stats_controller.cr
@@ -2,19 +2,56 @@ class StatsController < ApplicationController
   DATES = ["month", "day", "hour"]
 
   def stats
-    hash = {} of String => Array(String) | Array(Array(Float64) | Array(String)) | Array(String) | Record | Bool
+    u = @u.as(User)
+    halt!(422) if Record.empty_by(u)
+
     after_date  = date_params["after_date"]
     before_date = date_params["before_date"]
+    period      = date_params["period"]
 
-    DATES.each do |period|
-      pr = Record.filter_records(@u.as(User), period, after_date, before_date)
-      dates   = pr.map {|r| r.start_date.to_s}
-      amounts = pr.map {|r| r.amount.as(Float64)}
-      hash[period] = [amounts, dates]
+    last_date = Record.last_date(u)
+    first_date = Record.first_date(u)
+
+    qHour, qDay, qMonth = Record.counts(u)
+    tHour, tDay, tMonth = Record.total(u)
+
+
+    if after_date == "start" && before_date == "start"
+      before_date = last_date.to_s
+      after_date  = (last_date.as(Time) - 24.hours).to_s
+      period = "hour"
     end
-        
-    respond_with do 
-      json hash.to_json
+
+
+    pr = Record.filter_records(@u.as(User), "day", after_date, before_date)
+    dates   = pr.map {|r| r.start_date.to_s}
+    amounts = pr.map {|r| r.amount.as(Float64)}
+
+    res = JSON.build do |json|
+      json.object do
+        json.field "data" do 
+          json.object do
+            json.field "dates", dates
+            json.field "amounts", amounts
+          end
+        end
+
+        json.field "range" do
+          json.object do
+            json.field "last_date", last_date
+            json.field "first_date", first_date
+          end
+        end
+
+        json.field "statistics" do
+          json.object do
+            json.field "avgHour", tHour / qHour
+            json.field "avgDay", tDay / qDay
+            json.field "avgMonth", tMonth / qMonth
+            json.field "total", tDay
+          end
+        end
+      end
     end
   end
 
@@ -30,6 +67,7 @@ class StatsController < ApplicationController
     params.validation do
       required(:before_date) {|v| !v.empty?}
       required(:after_date) {|v| !v.empty?}
+      optional(:period) {|v| !v.empty?}
     end
   end
 end
--- src/models/record.cr
+++ src/models/record.cr
@@ -19,4 +19,38 @@ class Record < Crecto::Model
           )
       )
   end 
+
+  def self.last_date(user : User)
+    Repo.all(Record, 
+      Query.select(["start_date"]
+      ).where(user_id: user.id
+      ).order_by("start_date DESC"
+      ).limit(1)
+    )[0].start_date
+  end
+
+  def self.first_date(user : User)
+    Repo.all(Record, 
+      Query.select(["start_date"]
+      ).where(user_id: user.id
+      ).order_by("start_date ASC"
+      ).limit(1)
+    )[0].start_date
+  end
+
+  def self.empty_by(u : User)
+    Repo.all(Record, Query.where(user_id: u.id)).empty?
+  end
+
+  def self.total(u : User)
+    ["hour", "day", "month"].map do |period| 
+      Repo.aggregate(Record, :sum, :amount, Query.where(period: period, user_id: u.id)).to_s.to_f
+    end
+  end
+
+  def self.counts(u : User)
+    ["hour", "day", "month"].map do |period| 
+      Repo.aggregate(Record, :count, :id, Query.where(period: period, user_id: u.id)).to_s.to_i
+    end
+  end
 end
